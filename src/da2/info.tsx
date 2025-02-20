2267ad59839ed65494c6f5108e02f4cf78a016f3
song-dada <alf318@naver.com>
2025-02-21 00:52:52 +0900

    info부분 컨포넌트 분할, 범례부분 분할

--- src/da2/info.tsx
+++ src/da2/info.tsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useRef} from 'react';
 // import './sass/lay.scss';
 import CircleP from '../dageom/gpt/circle';
 import { IoMdArrowRoundBack } from "react-icons/io";
+import LegendRow from './legendRow';
 
 
 function DetailInfo( {onShow, onClose}:any ) {
@@ -16,7 +17,7 @@ function DetailInfo( {onShow, onClose}:any ) {
         <div className={`detailInfo`} ref={showObj}>
             일단 이게 위로 올라오고 내려오는???
             <button type="button" className="closeBtn" onClick={()=> onClose() }>close</button>
-            {/* <LegendRow/> */}
+            <LegendRow/>
         </div>
     )
 }
@@ -26,22 +27,13 @@ function InfoItem({sjarla}:any):any {
     return(
         <>
             <div className="infoItem">
-                {/* <progress id={"PM25"} value={'null'} max={1}></progress>
-                <progress value={data} /> */}
-                {/* <div className="progress-container">
-                    <div className="progress-background" />
-                    <div
-                        className="progress-bar"
-                        style={{ transform: `rotate(${(percentage / 100) * 180 - 90}deg)` }}
-                    />
-                    <div className="progress-text">{percentage}%</div>
-                </div> */}
                 <CircleP onOneRow={ sjarla }/>
             </div>
         </>
     )
 }
-function Info( {onMode, onOneRow }:any ) {
+function Info( {onMode, onOneRow, nowSido, nowStation }:any ) {
+    console.log(onOneRow);
     const [show, setShow] = useState<string>('100%');
     const upNdown=()=>{
         let up = '0';
@@ -59,41 +51,30 @@ function Info( {onMode, onOneRow }:any ) {
             return '100%';
         });
     }
-    // if(onMode){
-    //     return null;
-    // }
-    // else{
+    if(!onMode){
+        return null;
+    }
+    else{
         return(
             <>
                 <div className={`infoA ${onMode}`}>
                     <div className="address">
                         <div>
-                            <h2>지역 지역</h2>
-                            <h3>날짜 날짜</h3>
-                            <p>세부지역 위치는 어디라고 해야하지</p>
-
+                            <h2>{nowSido} {nowStation}</h2>
+                            {/* <h3>날짜 날짜</h3> */}
+                            <p>{onOneRow?.addr}</p>
                         </div>
                         <button type="button"><IoMdArrowRoundBack /></button>
                     </div>
-                    <div className={`info`}>
-                    <h3>오늘의 미세먼지</h3>
-                        <InfoItem sjarla={onOneRow}></InfoItem>
-                        {/* <div className="infoItem" onClick={ ()=> { upNdown(); } }> 데이터 1 </div> */}
-                        <div className="infoItem" onClick={ ()=> { upNdown(); } }> 데이터 2 </div>
-                    <h3>오늘의 대기질</h3>
-                        <div className="infoItem" onClick={ ()=> { upNdown(); } }> 데이터 3 </div>
-                        <div className="infoItem" onClick={ ()=> { upNdown(); } }> 데이터 4 </div>
-                        <div className="infoItem" onClick={ ()=> { upNdown(); } }> 데이터 5 </div>
-                        <div className="infoItem" onClick={ ()=> { upNdown(); } }> 데이터 6 </div>
-                    </div>
-                    {/* <div className="legend">범례</div> */}
-                    {/* <LegendRow/> */}
+                        <InfoItem sjarla={onOneRow} onEvent={ upNdown }></InfoItem>
+                     
+                    <LegendRow/>
                     <DetailInfo onShow={show} onClose={ close }/>
                 </div>
             
             </>
         )
-    // }
+    }
 }
 // function LegendRow(){
 //     return(
